9a0f99321be0cf493f088c8a8c6aed0516c02369
sam <weisen.li@hotmail.com>
2024-12-30 21:56:31 -0500

    merge with main

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -6,6 +6,8 @@ import {Locale, routing} from '@/i18n/routing';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,7 +46,9 @@ export default async function LocaleLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NextIntlClientProvider messages={messages}>
+          <Header />
           {children}
+          <Footer />
         </NextIntlClientProvider>
       </body>
     </html>
