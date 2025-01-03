ac043d6b9ba8c2cf497aa0499b5d172596dc9465
sam <weisen.li@hotmail.com>
2025-01-03 10:21:57 -0500

    fix logo and books page

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 const Logo = () => {
   return (
     <div className="flex items-center justify-center">
-      <img src="/logo.png" alt="logo" className="h-10" />
+      <Image src="/logo.png" alt="logo" className="h-10 object-contain" width={100} height={100} />
     </div>
   );
 };
