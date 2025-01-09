87bb5105144d8f72d63bc5e0968aa31a2809cf19
sam <weisen.li@hotmail.com>
2025-01-08 20:40:54 -0500

    group website page

--- app/[locale]/(website)/layout.tsx
+++ app/[locale]/(website)/layout.tsx
@@ -5,9 +5,9 @@ import {Locale, routing} from '@/i18n/routing';
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import "../globals.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
