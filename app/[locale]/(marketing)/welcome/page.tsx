2dd4fe8c1a0010a18e188f8d24e4a837f82ce17b
Sam Li <weisen.li@hotmail.com>
2025-02-10 16:43:32 +0000

    fix width for beloved section

--- app/[locale]/(marketing)/welcome/page.tsx
+++ app/[locale]/(marketing)/welcome/page.tsx
@@ -327,7 +327,7 @@ export default function LandingPage() {
       {/* Testimonials Section */}
       <div className="bg-pink-100 mx-auto px-4 py-20">
         <h2 className="text-5xl font-bold text-center mb-8"><span className="text-blue-500">Beloved</span> By Early Testers</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
           {[{
             tl: "My daughter wants me to read this book to her all the time! It’s the first time she has ever been the hero of a story, and she loves showing it to everyone.",
             desc: "LIli‘s mother",
