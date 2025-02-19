6f04cb2b9275cfa4afd3e889379187f58ea20885
Sam Li <weisen.li@hotmail.com>
2025-02-19 05:07:23 +0000

    style beloved section

--- app/[locale]/(marketing)/welcome/page.tsx
+++ app/[locale]/(marketing)/welcome/page.tsx
@@ -11,6 +11,7 @@ import ExpertlyCrafted from "../components/ExpertlyCrafted";
 import FAQWelcome from "../components/FAQWelcome";
 import EmailForm from "../components/EmailForm";
 import {AnimatedSection} from "@/app/components/AnimatedSection";
+import DreamzeImage from "@/app/components/DreamzeImage";
 
 
 export default function LandingPage() {
@@ -105,8 +106,8 @@ export default function LandingPage() {
       <AnimatedSection className="bg-blue-50 py-16 bg-[url(/welcome/about-us-bg.png)] bg-cover h-screen flex items-center">
         <div className="container mx-auto px-4">
           <h2 className="text-5xl font-bold text-center mb-8">About Us</h2>
-          <p className="text-lg max-w-2xl mx-auto text-center text-gray-700 mb-8">
-            The heart of Dreamaze is simple: <b>everyone deserves to be the hero of their own story.</b> As a mother I witnessed the pure joy on my daughter&apos;s face when she saw herself in a book—a unique experience she had never had before. We believe personalized books should go beyond pieced-together avatars to create immersive experiences where you or your loved ones are the true protagonists.
+          <p className="text-lg max-w-3xl mx-auto text-center text-[#222222] mb-8 font-light">
+            The heart of Dreamaze is simple: <b>everyone deserves to be the hero of their own story.</b> <br/>As a mother <br/>I witnessed the pure joy on my daughter&apos;s face when she saw herself in a book—a unique experience she had never had before. We believe personalized books should go beyond pieced-together avatars to create immersive experiences where you or your loved ones are the true protagonists.
           </p>
         </div>
       </AnimatedSection>
@@ -115,25 +116,27 @@ export default function LandingPage() {
         <h2 className="text-5xl font-bold text-center mb-8"><span className="text-blue-500">Beloved</span> By Early Testers</h2>
         <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
           {[{
-            tl: "My daughter wants me to read this book to her all the time! It's the first time she has ever been the hero of a story, and she loves showing it to everyone.",
-            desc: "LIli's mother",
+            desc: "My daughter wants me to read this book to her all the time! It's the first time she has ever been the hero of a story, and she loves showing it to everyone.",
+            tl: "LIli's mother",
             img: '/welcome/beloved-by-early-testers/lIli-mother.png'
           },
           {
-            tl: "Seeing my own child as the hero of the story is such a wonderful idea! I love how this book captures precious moments—I'll treasure it as a keepsake of all their milestones.",
-            desc: "Absalom's father",
+            desc: "Seeing my own child as the hero of the story is such a wonderful idea! I love how this book captures precious moments—I'll treasure it as a keepsake of all their milestones.",
+            tl: "Absalom's father",
             img: '/welcome/beloved-by-early-testers/absalom-father.png'
           },
           {
-            tl: "This is the perfect gift for my grandson! The story is heartwarming, the illustrations are beautiful, and most importantly, my little cutie looks amazing in the book. I can't wait to create one for all of my grandkids！",
-            desc: "Aaron's grandma",
+            desc: "This is the perfect gift for my grandson! The story is heartwarming, the illustrations are beautiful, and most importantly, my little cutie looks amazing in the book. I can't wait to create one for all of my grandkids！",
+            tl: "Aaron's grandma",
             img: '/welcome/beloved-by-early-testers/aaron-grandma.png'
           }].map(({tl, desc, img}) => 
-            <div className="text-center bg-white p-5" key={tl}>
-              <p className="text-gray-600">{tl}</p>
-              <div className="flex justify-center gap-1 items-center mt-4">
-                <Image src={img} height={48} width={48} alt={desc} />
-                <p className="text-gray-800 font-semibold">{desc}</p>
+            <div className="bg-white" key={tl}>
+              <div className="w-full relative aspect-square">
+                <DreamzeImage src={'/welcome/the-only-book/melody.png'} alt={desc} />
+                <p className="text-white font-bold text-2xl absolute z-10 bottom-0 w-full p-6 left-0">{tl}</p>
+              </div>
+              <div className="p-6">
+                <p className="text-[#222222] font-light">{desc}</p>
               </div>
             </div>
           )}
