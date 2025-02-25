273422573a448b22a00897f70c6288b3629d4d72
sam <weisen.li@hotmail.com>
2025-02-25 11:09:32 -0500

    update preview books content

--- app/[locale]/(marketing)/welcome/components/Previews.tsx
+++ app/[locale]/(marketing)/welcome/components/Previews.tsx
@@ -5,8 +5,8 @@ import { ContainerTitle } from "../../components/ContainerTitle";
 
 const PREVIEWS = [
   {
-    tl:'',
-    desc:'',
+    tl:'Goodnight to you',
+    desc: 'Ease your little one into peaceful dreams with Good Night to You. On a magical journey through sleep, gentle animals remind them of bedtime’s beauty and calm. This personalized story sparks imagination and creates the perfect, soothing end to the day.',
     thumbnail:'/welcome/book-previews/preview1.png',
     img:{
       desktop:'/welcome/book-previews/preview_good_night.png',
@@ -14,8 +14,8 @@ const PREVIEWS = [
     }
   },
   {
-    tl:'',
-    desc:'',
+    tl:'Santa',
+    desc:'Celebrate the magic of kindness and Christmas with a personalized letter from Santa! His warm words show how your joy and kindness create holiday magic. A cherished keepsake to treasure year after year!',
     thumbnail:'/welcome/book-previews/preview2.png',
     img:{
       desktop:'/welcome/book-previews/preview_santas_letter.png',
@@ -23,8 +23,8 @@ const PREVIEWS = [
     }
   },
   {
-    tl:'',
-    desc:'',
+    tl:'Name\'s melody',
+    desc:'Celebrate your baby’s name with Name’s Melody! Each letter comes to life through music, weaving a unique rhythm just for them. Open this enchanting story and discover their special melody!',
     thumbnail:'/welcome/book-previews/preview3.png',
     img:{
       desktop:'/welcome/book-previews/preview_melody.png',
@@ -32,8 +32,8 @@ const PREVIEWS = [
     }
   },
   {
-    tl:'',
-    desc:'',
+    tl:'Birthday Book',
+    desc:'In a whimsical forest, the animals are preparing a magical birthday just for you! As they gather surprises, they discover what makes you truly special. Inspired by their findings, they send special guests to celebrate. What wonders await? Open the book and step into your birthday adventure!',
     thumbnail:'/welcome/book-previews/preview4.png',
     img:{
       desktop:'/welcome/book-previews/preview_birthday.png',
@@ -41,8 +41,8 @@ const PREVIEWS = [
     }
   },
   {
-    tl:'',
-    desc:'',
+    tl:'Bravery',
+    desc:'Help your little one explore bravery through everyday moments, this heartfelt story shows that courage isn’t about being fearless but facing challenges in your own way. With warmth and reassurance, it inspires confidence and resilience, making it a cherished part of growing up.',
     thumbnail:'/welcome/book-previews/preview5.png',
     img:{
       desktop:'/welcome/book-previews/preview_bravery.png',
@@ -71,9 +71,9 @@ export default function Previews() {
       <div className="container mx-auto px-3">
         {PREVIEWS.map(({tl,desc,img:{desktop,mobile}},idx)=>
           <article key={idx} className="text-[#222222]">
-            <h3 className="font-bold text-[28px] text-center">Good Night Luna{tl}</h3>
+            <h3 className="font-bold text-[28px] text-center">{tl}</h3>
             <ContainerDesc>
-              By leaving a $1 deposit, you reserve the right to purchase this product at a discount when we launch. This is a binding agreement between you and Dreamaze Book. You retain the right to a refund until the product is delivered.{desc}
+              {desc}
             </ContainerDesc>
             <div className="relative w-full aspect-square md:aspect-[5/2] mt-6 mb-16">
               <DreamzeImage src={desktop} alt={tl} cssClass="hidden md:block" />
