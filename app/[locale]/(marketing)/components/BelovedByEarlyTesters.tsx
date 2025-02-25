a3c016d480914be92a21395d79516b112af79c46
sam <weisen.li@hotmail.com>
2025-02-25 10:50:29 -0500

    fix beloved by testers images

--- app/[locale]/(marketing)/components/BelovedByEarlyTesters.tsx
+++ app/[locale]/(marketing)/components/BelovedByEarlyTesters.tsx
@@ -12,26 +12,26 @@ export default function BelovedByEarlyTesters() {
           <span><span className="text-[#022CCE]">Beloved</span> By Early Testers</span>
           <Image src="/welcome/beloved-by-early-testers/like.png" alt="Like" width={64} height={64} />
         </ContainerTitle>
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-12">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 px-2.5 md:px-0 gap-24 md:gap-12">
         {[{
-          desc: "My daughter wants me to read this book to her all the time! It’s the first time she has ever been the hero of a story, and she loves showing it to everyone.",
-          tl: "LIli‘s mother",
+          desc: "The moment I saw the melody book with my son’s face in it, I was truly moved. Seeing the instruments come together, carrying the unique blessing behind his name, felt so personal and magical. It’s such a beautiful and meaningful gift!",
+          tl: "Elija’s Mom",
           img: '/welcome/beloved-by-early-testers/lili-mother.png',
-          cssClass: 'bg-[#FFF2F2]'
+          cssClass: 'bg-[url(/welcome/beloved-by-early-testers/lili-mother-bg-app.png)] md:bg-[url(/welcome/beloved-by-early-testers/lili-mother-bg.png)]'
         },
         {
-          desc: "Seeing my own child as the hero of the story is such a wonderful idea! I love how this book captures precious moments—I’ll treasure it as a keepsake of all their milestones.",
-          tl: "Mirabelle’s father",
+          desc: "Seeing my own child as the hero of the story is such a wonderful idea! We love how this book captures precious moments—we’ll treasure it as a keepsake of all his milestones.",
+          tl: "Ethan’s Dad",
           img: '/welcome/beloved-by-early-testers/mirabelle-father.png',
-          cssClass: 'bg-[#FFFBF3]'
+          cssClass: 'bg-[url(/welcome/beloved-by-early-testers/mirabelle-father-bg-app.png)] md:bg-[url(/welcome/beloved-by-early-testers/mirabelle-father-bg.png)]'
         },
         {
-          desc: "This is the perfect gift for my grandson! The story is heartwarming, the illustrations are beautiful, and most importantly, my little cutie looks amazing in the book. I can’t wait to create one for all of my grandkids！",
-          tl: "Aaron‘s grandma",
+          desc: "This is the perfect gift for my granddaughter! The story is heartwarming, the illustrations are beautiful, and most importantly, my little cutie looks amazing in the book. I can’t wait to create one for all of my grandkids！",
+          tl: "Amelia’s grandma",
           img: '/welcome/beloved-by-early-testers/aaron-grandma.png',
-          cssClass: 'bg-[#F2F9FF]'
+          cssClass: 'bg-[url(/welcome/beloved-by-early-testers/aaron-grandma-bg-app.png)] md:bg-[url(/welcome/beloved-by-early-testers/aaron-grandma-bg.png)]'
         }].map(({tl, desc, img, cssClass}) => 
-          <div className={`relative ${cssClass}`} key={tl}>
+          <div className={`relative bg-cover  ${cssClass}`} key={tl}>
             <Image src={img} alt={tl} width={120} height={120} className="absolute mx-auto -top-14 left-0 right-0" />
             <div className="p-9">
               <h3 className="font-bold text-[#222222] text-[28px] text-center mt-10 mb-4">{tl}</h3>
