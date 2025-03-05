829b59d648b43b90019fabb48a2f34b60717237d
Sam Li <weisen.li@hotmail.com>
2025-03-05 21:09:58 +0000

    fix thankyou page

--- app/[locale]/(marketing)/welcome/thankyou/page.tsx
+++ app/[locale]/(marketing)/welcome/thankyou/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import DreamzeImage from "@/app/components/DreamzeImage";
 import Link from "next/link";
 
 export default function Thankyou() {
@@ -12,7 +11,7 @@ export default function Thankyou() {
       
       <div className='bg-[#F5E3E3] flex justify-center items-center h-screen'>
         <div className='bg-white px-[88px] pt-12 pb-16 text-[#222222] max-w-4xl mx-auto'>
-          <h1 className='font-bold text-[40px] text-center'>Congrats! You're a VIP!</h1>
+          <h1 className='font-bold text-[40px] text-center'>Congrats! You&rsquo;re a VIP!</h1>
           <p className='text-xl font-light mt-6 mb-3'>Thank you so much for reserving your spot! You’re locked in to receive the first-day best discount and VIP-only perks when you back our project on Kickstarter!</p>
           <p className='text-xl font-light mb-6'>Stay tuned – you’ll receive an email from us soon with all the details about the launch.</p>
           <div className='flex justify-center items-center'>
