0beb4c8975bd4a9afa72e2712867d8af9362c42b
Sam Li <weisen.li@hotmail.com>
2025-03-15 14:12:17 +0000

    add header to privacy policy

--- app/[locale]/(marketing)/welcome/privacy-policy/page.tsx
+++ app/[locale]/(marketing)/welcome/privacy-policy/page.tsx
@@ -1,5 +1,12 @@
 import PrivacyPolicy from "@/app/components/PrivacyPolicy";
+import Link from "next/link";
+import Image from 'next/image';
 
 export default function PrivacyPolicyPage() {
-  return <section className="max-w-7xl mx-auto text-[#222222] p-5"><PrivacyPolicy /></section>
+  return <section className="max-w-7xl mx-auto text-[#222222] p-5">
+    <Link href={'/'} className="block mx-auto mb-5">
+        <Image className="" src={'/welcome/dreamaze-logo.png'} alt="Logo" width={168} height={56} />
+      </Link>
+    <PrivacyPolicy />
+  </section>
 }
