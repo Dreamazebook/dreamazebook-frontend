a4b90b636155dfe419908afe3098390633d9b03d
Sam Li <weisen.li@hotmail.com>
2025-03-15 13:27:07 +0000

    fix privacy policy padding

--- app/[locale]/(marketing)/welcome/privacy-policy/page.tsx
+++ app/[locale]/(marketing)/welcome/privacy-policy/page.tsx
@@ -1,5 +1,5 @@
 import PrivacyPolicy from "@/app/components/PrivacyPolicy";
 
 export default function PrivacyPolicyPage() {
-  return <section className="max-w-7xl mx-auto text-[#222222]"><PrivacyPolicy /></section>
+  return <section className="max-w-7xl mx-auto text-[#222222] p-5"><PrivacyPolicy /></section>
 }
