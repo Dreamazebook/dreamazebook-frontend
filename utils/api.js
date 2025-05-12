ef9f2dc0beba0c3b8f838c539f2eeb8bc150e19d
Sam Li <weisen.li@hotmail.com>
2025-05-12 17:31:01 +0000

    add api url to environment

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+
 const api= axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: BASE_URL,
     withCredentials: true,
     timeout: 10000, // 请求超时时间设置为 10 秒
     headers: {
