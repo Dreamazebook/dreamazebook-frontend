a2308d4cdec58d69ca8cdd9222191911bb4b1dd8
Sam Li <weisen.li@hotmail.com>
2025-06-25 14:16:14 +0000

    update redirect url

--- app/[locale]/(marketing)/welcome/page.tsx
+++ app/[locale]/(marketing)/welcome/page.tsx
@@ -37,7 +37,7 @@ export default function LandingPage() {
       </AnimatedSection>
 
       <AnimatedSection>
-        <ReserveSection btnId="email_submit_mid" redirectUrl={'/en/welcome/reserve'} cssClass={"bg-[#F5E3E3]"} title={"Let Your Child Shine as the Hero"} desc={'Submit your email to receive updates and exclusive early bird perks'} btnText="Unlock Early Access & Perks!" />
+        <ReserveSection btnId="email_submit_mid" redirectUrl={'/en/welcome/success'} cssClass={"bg-[#F5E3E3]"} title={"Let Your Child Shine as the Hero"} desc={'Submit your email to receive updates and exclusive early bird perks'} btnText="Unlock Early Access & Perks!" />
       </AnimatedSection>
 
       <AnimatedSection>
@@ -53,7 +53,7 @@ export default function LandingPage() {
       </AnimatedSection>
 
       <AnimatedSection>
-        <ReserveSection btnId="email_submit_3rd" redirectUrl={'/en/welcome/reserve'} cssClass={""} desc="Leave your email now to unlock behind-the-scenes peeks into our creation process — from sketch to storybook." btnText="Sign Up& Shape the Story With Us" />
+        <ReserveSection btnId="email_submit_3rd" redirectUrl={'/en/welcome/success'} cssClass={""} desc="Leave your email now to unlock behind-the-scenes peeks into our creation process — from sketch to storybook." btnText="Sign Up& Shape the Story With Us" />
       </AnimatedSection>
 
       <AboutUs />
@@ -71,7 +71,7 @@ export default function LandingPage() {
       <AnimatedSection>
         <ReserveSection
           btnId="email_submit_footer"
-          redirectUrl={'/en/welcome/reserve'}
+          redirectUrl={'/en/welcome/success'}
           style={{'--vip-discount-app':`url(${VIP_DISCOUNT_APP})`,'--vip-discount':`url(${VIP_DISCOUNT})`} as React.CSSProperties}
           //cssClass={"text-white bg-(image:--vip-discount-app) md:bg-(image:--vip-discount)"}
           title={"Your Smartest Save Yet!"}
