41c829827bb31d706fa6e30444b4359063d70d4f
Sam Li <weisen.li@hotmail.com>
2025-07-23 05:10:42 +0000

    do not set default address to form

--- app/[locale]/(website)/checkout/components/ShippingForm.tsx
+++ app/[locale]/(website)/checkout/components/ShippingForm.tsx
@@ -64,22 +64,21 @@ const ShippingForm: React.FC<ShippingFormProps> = ({
   
   // 当选择已保存的地址时，自动填充表单字段
   useEffect(() => {
-    console.log(addressList);
-    if (selectedAddressId) {
-      const selectedAddress = addressList.find(addr => addr.id === selectedAddressId);
-      if (selectedAddress) {
-        // 填充表单字段
-        if (selectedAddress.email) setEmail(selectedAddress.email);
-        if (selectedAddress.first_name) setFirstName(selectedAddress.first_name);
-        if (selectedAddress.last_name) setLastName(selectedAddress.last_name);
-        if (selectedAddress.street) setAddress(selectedAddress.street);
-        if (selectedAddress.city) setCity(selectedAddress.city);
-        if (selectedAddress.postal_code) setPostalcode(selectedAddress.postal_code);
-        if (selectedAddress.country) setCountry(selectedAddress.country);
-        if (selectedAddress.state) setState(selectedAddress.state);
-        if (selectedAddress.phone) setPhone(selectedAddress.phone);
-      }
-    }
+    // if (selectedAddressId) {
+    //   const selectedAddress = addressList.find(addr => addr.id === selectedAddressId);
+    //   if (selectedAddress) {
+    //     // 填充表单字段
+    //     if (selectedAddress.email) setEmail(selectedAddress.email);
+    //     if (selectedAddress.first_name) setFirstName(selectedAddress.first_name);
+    //     if (selectedAddress.last_name) setLastName(selectedAddress.last_name);
+    //     if (selectedAddress.street) setAddress(selectedAddress.street);
+    //     if (selectedAddress.city) setCity(selectedAddress.city);
+    //     if (selectedAddress.postal_code) setPostalcode(selectedAddress.postal_code);
+    //     if (selectedAddress.country) setCountry(selectedAddress.country);
+    //     if (selectedAddress.state) setState(selectedAddress.state);
+    //     if (selectedAddress.phone) setPhone(selectedAddress.phone);
+    //   }
+    // }
   }, [selectedAddressId, addressList]);
   const clearError = (field: keyof ShippingErrors) => {
     if (errors[field]) {
