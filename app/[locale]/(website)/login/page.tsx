e8428361cef3b179ec75fb3e955a31ef94c319f1
Sam Li <weisen.li@hotmail.com>
2025-07-26 21:10:37 +0000

    initial login page

---
+++ app/[locale]/(website)/login/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { useState } from 'react';
+import LoginModal from '../components/LoginModal';
+
+export default function LoginPage() {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <LoginModal />
+    </div>
+  );
+}
