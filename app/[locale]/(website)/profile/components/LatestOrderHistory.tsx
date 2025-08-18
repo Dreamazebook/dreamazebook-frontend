342332fc841ef41d31769bef052e9c7e61a54699
Sam Li <weisen.li@hotmail.com>
2025-08-18 03:57:10 +0000

    refactor orderSummaryDelivery component

--- app/[locale]/(website)/profile/components/LatestOrderHistory.tsx
+++ app/[locale]/(website)/profile/components/LatestOrderHistory.tsx
@@ -1,33 +1,23 @@
 "use client";
 
-import { formatAddress } from "@/types/address";
 import { OrderDetail } from "../../checkout/components/types";
 import DisplayPrice from "../../components/component/DisplayPrice";
 import OrderItem from "../../components/component/OrderItem";
-import { formatDate } from "@/app/[locale]/admin/orders/utils";
+import OrderSummaryDelivery from "../../components/component/OrderSummaryDelivery";
 
 export default function LatestOrderHistory({ orderDetail }:{orderDetail:OrderDetail}) {
   const {order_number,status,shipping_address,total_amount,items,updated_at} = orderDetail;
 
   return (
     <div key={order_number} className="border rounded-lg p-3 md:p-4">
-      <div className="flex flex-col sm:flex-row sm:items-center mb-3 gap-2">
-        <span className="text-base md:text-lg font-medium text-gray-900">{order_number}</span>
+      <div className="flex flex-col sm:flex-row sm:items-center mb-5 gap-2">
+        <span className="text-base md:text-lg font-medium text-gray-900">#{order_number}</span>
         <span className="bg-green-100 text-green-800 px-2 py-1 rounded text-xs font-medium w-fit capitalize">
           {status}
         </span>
       </div>
       
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-sm text-[#222] mb-4 bg-[#F8F8F8] p-3">
-        <div className="space-y-3">
-          <div><span className="font-medium text-[#999] mr-4">Ship to</span> {formatAddress(shipping_address)}</div>
-          <div><span className="font-medium text-[#999] mr-4">Order date</span> {formatDate(updated_at)}</div>
-          {/* <div><span className="font-medium text-[#999] mr-4">Qty</span> 1</div> */}
-        </div>
-        <div>
-          <div><span className="font-medium text-[#999] mr-4">Delivery date</span> </div>
-        </div>
-      </div>
+      <OrderSummaryDelivery orderDetail={orderDetail} />
 
       <div className="space-y-3">
         
