d66647284f8bd77a3723dbb4441d18e12fc0a3ca
Dreamaze book <dreamazebook@gmail.com>
2025-09-26 03:26:33 +0000

    add filter order status

--- app/[locale]/admin/orders/hooks/useOrdersData.ts
+++ app/[locale]/admin/orders/hooks/useOrdersData.ts
@@ -6,15 +6,16 @@ import { API_ADMIN_ORDERS } from '@/constants/api';
 import { ApiResponse } from '@/types/api';
 import { OrderDetail } from '@/app/[locale]/(website)/checkout/components/types';
 
-export const useOrdersData = () => {
+export const useOrdersData = (filters: any) => {
   const [orders, setOrders] = useState<OrderDetail[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchOrders = async () => {
+      setLoading(true);
       try {
-        const { data, code, success } = await api.get<ApiResponse>(API_ADMIN_ORDERS);
+        const { data, code, success } = await api.get<ApiResponse>(`${API_ADMIN_ORDERS}?status=${filters.statusFilter}`);
         if (success) {
           setOrders(data);
         }
@@ -27,7 +28,7 @@ export const useOrdersData = () => {
     };
 
     fetchOrders();
-  }, []);
+  }, [filters]);
 
   return { orders, loading, error, setOrders };
 };
