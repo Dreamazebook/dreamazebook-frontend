77e6360788ceb4c3ec21dcc7495e48fe2ce7d5fb
Sam Li <weisen.li@hotmail.com>
2025-10-22 13:41:25 +0000

    update login/register api

--- constants/api.ts
+++ constants/api.ts
@@ -1,8 +1,8 @@
 export const API_DOMAIN = process.env.NEXT_PUBLIC_API_URL || 'https://api.dreamazebook.com/api';
 
-export const API_USER_REGISTER = API_DOMAIN + '/auth/register';
+export const API_USER_REGISTER = API_DOMAIN + '/register';
 
-export const API_USER_LOGIN = API_DOMAIN + '/auth/login';
+export const API_USER_LOGIN = API_DOMAIN + '/login';
 export const API_USER_LOGOUT = API_DOMAIN + '/auth/logout';
 
 export const API_USER_SEND_PASSWORD_RESET_EMAIL = API_DOMAIN + '/auth/forgot-password';
