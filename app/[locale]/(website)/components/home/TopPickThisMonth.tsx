d325ce2021df1105f5711fbb9373429762ac12fd
Dreamazebook <dreamazebook@gmail.com>
2025-12-07 22:31:33 +0000

    style top pick this month

--- app/[locale]/(website)/components/home/TopPickThisMonth.tsx
+++ app/[locale]/(website)/components/home/TopPickThisMonth.tsx
@@ -83,7 +83,7 @@ const TopPickThisMonth = () => {
                   }}
                 >
                   <div
-                    className={`w-full h-full bg-white rounded-lg shadow-2xl p-3 md:p-4 ${
+                    className={`w-full h-full bg-white rounded shadow ${
                       isClient && window.innerWidth >= 768 ? (
                         isPrev && isAnimating
                           ? 'animate-image-exit'
@@ -108,7 +108,7 @@ const TopPickThisMonth = () => {
             })}
           </div>
 
-          <div className="absolute -bottom-8 left-1/2 -translate-x-1/2 flex gap-2">
+          {/* <div className="absolute -bottom-8 left-1/2 -translate-x-1/2 flex gap-2">
             {images.map((_, index) => (
               <button
                 key={index}
@@ -125,7 +125,7 @@ const TopPickThisMonth = () => {
                 aria-label={`Go to slide ${index + 1}`}
               />
             ))}
-          </div>
+          </div> */}
         </div>
 
         <div className="text-center lg:text-left space-y-6 mt-12 lg:mt-0 rounded">
