08a9d6bdf46478e7e0d4e7979475b7be6935b2e5
Dreamazebook <dreamazebook@gmail.com>
2026-04-14 20:00:21 +0000

    reduce html for lastingMemorial component

--- app/[locale]/(website)/components/home/LastingMemorial.tsx
+++ app/[locale]/(website)/components/home/LastingMemorial.tsx
@@ -43,38 +43,34 @@ export default function LastingMemorial() {
   };
 
   return (
-    <section className="flex flex-col items-center px-[24px] py-[64px] md:py-[88px] bg-cover bg-center bg-white">
-      <header className="flex items-center justify-center gap-2.5 pb-6 w-full">
-        <h1 className="flex-1 font-semibold text-[#222222] text-2xl md:text-4xl text-center tracking-[0] leading-8" style={{ fontFamily: "Roboto, Helvetica" }}>
-          Easy 3 Steps to Create a Lasting Memory
-        </h1>
-      </header>
+    <section className="px-[24px] py-[64px] md:py-[88px]">
+      <h2 className="pb-6 flex-1 font-semibold text-[#222222] text-2xl md:text-4xl text-center tracking-[0] leading-8" style={{ fontFamily: "Roboto, Helvetica" }}>
+        Easy 3 Steps to Create a Lasting Memory
+      </h2>
 
-      <div className="flex flex-col items-center w-full max-w-[375px] md:max-w-[1000px]">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-12 items-start gap-12 pt-9">
-          {steps.map((step, index) => (
-            <article
-              key={index}
-              className="flex md:flex-col items-start gap-6 pl-6 pr-0 w-full border-l border-solid border-[#22222233]"
-            >
-              <Image
-                className="w-12 h-11 flex-shrink-0"
-                alt={`Step ${step.number} icon`}
-                src={step.icon}
-              />
+      <div className=" w-full max-w-[375px] md:max-w-[1000px] grid grid-cols-1 md:grid-cols-3 gap-12 items-start gap-12 pt-9 mx-auto">
+        {steps.map((step, index) => (
+          <article
+            key={index}
+            className="flex md:flex-col items-start gap-6 pl-6 pr-0 w-full border-l border-solid border-[#22222233]"
+          >
+            <Image
+              className="w-12 h-11 flex-shrink-0"
+              alt={`Step ${step.number} icon`}
+              src={step.icon}
+            />
 
-              <div className="flex flex-col items-start gap-1 pr-3 flex-1">
-                <h2 className="w-fit text-[#222222] whitespace-nowrap" style={m3Styles.titleMedium}>
-                  {step.title}
-                </h2>
+            <div className="flex flex-col items-start gap-1 pr-3 flex-1">
+              <h2 className="w-fit text-[#222222] whitespace-nowrap" style={m3Styles.titleMedium}>
+                {step.title}
+              </h2>
 
-                <p className="text-[#666666]" style={m3Styles.bodyMedium}>
-                  {step.description}
-                </p>
-              </div>
-            </article>
-          ))}
-        </div>
+              <p className="text-[#666666]" style={m3Styles.bodyMedium}>
+                {step.description}
+              </p>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
   );
