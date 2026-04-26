f4720a6b3f88e6b94069522532b40f18a22b39c6
Dreamazebook <dreamazebook@gmail.com>
2026-04-26 13:32:02 +0000

    show/hide shopping cart banner logic

--- app/[locale]/(website)/(orders)/shopping-cart/page.tsx
+++ app/[locale]/(website)/(orders)/shopping-cart/page.tsx
@@ -294,7 +294,7 @@ export default function ShoppingCartPage() {
                   }}
                 />
 
-                <ShippingProgressBanner itemsCount={itemsCount} />
+                <ShippingProgressBanner itemsCount={selectedItems.length} />
               </div>
             )}
           </div>
