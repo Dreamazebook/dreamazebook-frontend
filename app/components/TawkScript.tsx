1d7abf0c722f57accf276df65e5e96eb719cbc9e
Dreamazebook <dreamazebook@gmail.com>
2026-05-23 15:28:39 +0000

    add tawk chat

---
+++ app/components/TawkScript.tsx
@@ -0,0 +1,23 @@
+import Script from 'next/script';
+
+export default function TawkScript() {
+  return (
+    <Script
+      id="tawk-script"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          (function(){
+          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/6a11c50e5f3e2e1c3435010d/1jpamjgnd';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+          })();
+        `,
+      }}
+    />
+  );
+}
